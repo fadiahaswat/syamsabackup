8fff6a945de6052039fc63e2489553303dd9dfc9
Nama Anda <andiaqillahfadiahaswat@gmail.com>
2026-06-30 17:28:37 +0700

    commit

--- src/config/config.local.js
+++ src/config/config.local.js
@@ -15,6 +15,6 @@ if (typeof window.APP_SECRETS === 'undefined') {
 // Set nilai-nilai default/placeholder (akan di-override oleh config.js)
 // Kosongkan atau isi sesuai kebutuhan deployment Anda
 window.APP_SECRETS.googleSheetUrl = "https://script.google.com/macros/s/AKfycbw-URYAsLTWCdnGurQhM1ZXa9N8vm-GBlHwtetDlin73-Ma8G0aAbFoboGGUI8GgVDl/exec";
-window.APP_SECRETS.googleClientId = "";
-window.APP_SECRETS.adminEmails = [];
+window.APP_SECRETS.googleClientId = "336443539411-b7uv4udqqhbqpdmeuja54dhfsda4q7cm.apps.googleusercontent.com";
+window.APP_SECRETS.adminEmails = ["andiaqillah@muallimin.sch.id"];
 window.APP_SECRETS.superadminHash = null;
